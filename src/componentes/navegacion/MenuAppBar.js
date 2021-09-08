5db1490b1f9afc170383b9347b9d547454da1295
PedroAvila <minimainor_austin@hotmail.com>
2021-09-08 12:05:33 -0500

    Responsive Desing

--- src/componentes/navegacion/MenuAppBar.js
+++ src/componentes/navegacion/MenuAppBar.js
@@ -1,22 +1,54 @@
-import { AppBar, Button, Container, Icon, Link, Toolbar, Typography } from '@material-ui/core';
-import React from 'react';
+import { AppBar, Button, Container, Drawer, Icon, IconButton, Link, List, ListItem, ListItemIcon, ListItemText, Toolbar, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
 import useStyles from '../../theme/useStyles';
 
 const MenuAppBar = () => {
+    const [ open, setOpen ] = useState(false)
     const classes = useStyles()
+
+    const openToggle = () => {
+        setOpen(true)
+    }
+
+    const closeToggle = () => {
+        setOpen(false)
+    }
+
 
     return (
         <div>
             <AppBar position="static" className={ classes.appBar }>
                 <Container>
                     <Toolbar>
+                        <div className={ classes.sectionMobile }>
+                            <IconButton color="inherit" onClick={ openToggle }>
+                                <Icon fontSize="large">menu</Icon>
+                            </IconButton>
+                        </div>
+                        <Drawer
+                        open={ open }
+                        onClose={ closeToggle }
+                        >
+                            <div className={ classes.list }>
+                                <List>
+                                    <ListItem button onClick={ closeToggle } className={ classes.listItem }>
+                                        <Link color="inherit" className={ classes.linkAppBarMobile } underline="none">
+                                            <ListItemIcon className={ classes.listItemIcon }>
+                                                <Icon>person</Icon>
+                                            </ListItemIcon>
+                                            <ListItemText>Login</ListItemText>
+                                        </Link>
+                                    </ListItem>
+                                </List>
+                            </div>    
+                        </Drawer>
                         <div className={ classes.grow }>
                             <Link color="inherit" className={ classes.linkAppBarLogo } underline="none">
                                 <Icon className={ classes.mr } fontSize="large">store</Icon>
                                 <Typography variant="h5">VAXI SHOP</Typography>
                             </Link>
                         </div>
-                        <div>
+                        <div className={ classes.sectionDesktop }>
                             <Button color="inherit" className={ classes.linkAppBarDesktop }>
                                 <Link color="inherit" classNaame={ classes.linkAppBarDesktop } underline="none">
                                     <Icon className={ classes.mr }>person</Icon>
