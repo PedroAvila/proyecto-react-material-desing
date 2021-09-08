89f0f2e79a6d6bc4890090c60031636b857f7f77
PedroAvila <minimainor_austin@hotmail.com>
2021-09-08 07:51:59 -0500

    Creación y Personalización del theme de Material Desing

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "@material-ui/core";
+import React from "react";
+import Login from "./componentes/seguridad/Login";
+import theme from "./theme/theme";
+
+function App() {
+  return (
+    <ThemeProvider theme={ theme }>
+      <Login />
+    </ThemeProvider> 
+  )
+}
+
+export default App;
