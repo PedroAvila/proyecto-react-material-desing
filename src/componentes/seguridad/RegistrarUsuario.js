a0958641f375ed2ef91e4d72a9075ff3e960a89d
PedroAvila <minimainor_austin@hotmail.com>
2021-09-08 18:31:51 -0500

    Formularios

--- src/componentes/seguridad/RegistrarUsuario.js
+++ src/componentes/seguridad/RegistrarUsuario.js
@@ -1,10 +1,37 @@
 
 import { Avatar, Button, Card, Container, Grid, Icon, TextField, Typography } from '@material-ui/core';
-import React from 'react';
+import React, {useState } from 'react';
 import useStyles from '../../theme/useStyles';
 import { Link } from 'react-router-dom';
 
+const clearUsuario = {
+    nombre: '',
+    apellidos: '',
+    email: '',
+    password: ''
+}
+
 const RegistrarUsuario = () => {
+    const [ usuario, setUsuario ] = useState({
+        nombre: '',
+        apellidos: '',
+        email: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setUsuario(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    const guardarUsuario = () => {
+        console.log("Mi usuario es", usuario)
+        setUsuario(clearUsuario)
+    }
+
     const classes = useStyles()
 
     return (
@@ -16,13 +43,16 @@ const RegistrarUsuario = () => {
                             <Icon className={ classes.icon }>person_add</Icon>
                         </Avatar>
                         <Typography variant="h5" color="primary">Registro de Usuario</Typography>
-                        <form className={ classes.form }>
+                        <form className={ classes.form } onSubmit={ (e) => e.preventDefault() }>
                             <Grid container spacing={2}>
                                 <Grid item md={6} xs={12} className={ classes.gridmb }>
                                     <TextField 
                                     label="Nombre"
                                     variant="outlined"
                                     fullWidth
+                                    name="nombre"
+                                    value={ usuario.nombre }
+                                    onChange={ handleChange }
                                     />
                                 </Grid>
 
@@ -31,6 +61,9 @@ const RegistrarUsuario = () => {
                                     label="Apellidos"
                                     variant="outlined"
                                     fullWidth
+                                    name="apellidos"
+                                    value={ usuario.apellidos }
+                                    onChange={ handleChange }
                                     />
                                 </Grid>
 
@@ -40,6 +73,9 @@ const RegistrarUsuario = () => {
                                     variant="outlined"
                                     fullWidth
                                     type="email"
+                                    name="email"
+                                    value={ usuario.email }
+                                    onChange={ handleChange }
                                     />
                                 </Grid>
 
@@ -49,6 +85,9 @@ const RegistrarUsuario = () => {
                                     variant="outlined"
                                     fullWidth
                                     type="password"
+                                    name="password"
+                                    value={ usuario.password }
+                                    onChange={ handleChange }
                                     />
                                 </Grid>
 
@@ -57,6 +96,8 @@ const RegistrarUsuario = () => {
                                     variant="contained"
                                     fullWidth
                                     color="primary"
+                                    onClick={ guardarUsuario }
+                                    type="submit"
                                     >
                                         Registrar
                                     </Button>
